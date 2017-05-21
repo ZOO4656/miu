05868990d60654c59f05c3da992872389c31078f
uenoryo <r.r.ag0611@outlook.jp>
2017-05-21 16:04:01 +0900

    基本コンポーネント作成

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>カフェ&居酒屋 miu</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<!-- ヘッダー画像 -->
+<?php require_once('components/header_img.php') ?>
+
+<!-- メニューバー -->
+<?php require_once('components/nav.php') ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?= get_header() ?>
+
+<!-- メニューリンク -->
+<?php require_once('components/menu_link.php') ?>
+
+<!-- お知らせ -->
+<?php require_once('components/news_list.php') ?>
+
+<!-- お店の紹介 -->
+<?php require_once('components/about_us.php') ?>
+
+<!-- カフェメニュー -->
+<?php require_once('components/menu_cafe.php') ?>
+
+<!-- ディナーメニュー -->
+<?php require_once('components/menu_dinner.php') ?>
+
+<!-- アクセス -->
+<?php require_once('components/access.php') ?>
+
+<?= get_footer() ?>
