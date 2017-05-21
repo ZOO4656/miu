289db0f94688aeabf35b076f396aef6537e07e63
uenoryo <r.r.ag0611@outlook.jp>
2017-05-21 16:38:25 +0900

    スタイルシートのURLを修正

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>カフェ&居酒屋 miu</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/css/main.css">
 </head>
 <body>
 
