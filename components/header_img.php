60e55d6e347132fbc5390ff2b19988da01875580
uenoryo <r.r.ag0611@outlook.jp>
2017-05-28 13:06:10 +0900

    ヘッダー画像 + ロゴ設置

---
+++ components/header_img.php
@@ -0,0 +1,13 @@
+<section class="HeaderImg">
+    <div class="HeaderImg__head">
+        <div class="MainLogoBox">
+            <div class="MainLogoBox__sub">
+                <h3 class="MainLogoBox__head">カフェ & 居酒屋</h3>
+                <h1 class="MainLogoBox__body">miu</h1>
+            </div>
+        </div>
+    </div>
+    <div class="HeaderImg__body">
+        <img src="<?= get_template_directory_uri() ?>/img/header_img_sample.png" alt="カフェ & 居酒屋　miu">
+    </div>
+</section>
