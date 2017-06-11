1c69ba58220128b5c37032190db44b30241c3266
kentaro <zoo4656keepkeep@gmail.com>
2017-06-11 19:04:31 +0900

    CategoryBoxListレスポンジ部化

--- components/menu_link.php
+++ components/menu_link.php
@@ -13,8 +13,7 @@
         <div class="CategoryBox">
             <h2 class="CategoryBox__head">Dinner</h2>
             <img class="CategoryBox__body" src="<?= get_template_directory_uri() ?>/img/IMG_1915.JPG" alt="イメージなし">
-            <P class="CategoryBox__foot"></P>
-            <P class="CategoryBox__foot">夜はお店の雰囲気を変えLunchではない居酒屋を行っています。</P>
+            <P class="CategoryBox__foot">夜はお店の雰囲気を変え居酒屋を行っています。</P>
             <P class="CategoryBox__foot">食材はもちろん地元の物を使用しているので安心です。</P>
             <P class="CategoryBox__foot">お仕事で疲れた体を癒やす栄養満点の料理</P>
             <P class="CategoryBox__foot">品数多く準備してお待ちしていますので是非ご来店ください。</P>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>カフェ&居酒屋 miu</title>
     <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/css/main.css">
 </head>
